936fe58ecb2fadd4de0c89eea4c81bc1301717dc
David Waza <wazabanga00@gmail.com>
2023-04-27 23:59:43 +0100

    created prisma schema

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,8 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import AuthForm from "@/components/AuthForm";
 
 const Signup = () => {
-  return(
-  <AuthForm mode='register' />
-
-  )
+  return <AuthForm mode="register" />;
 };
 
 export default Signup;
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,10 +1,7 @@
-
 import AuthForm from "@/components/AuthForm";
 
 const Login = () => {
-  return(
-    <AuthForm mode="signin" />
-  )
+  return <AuthForm mode="signin" />;
 };
 
 export default Login;
